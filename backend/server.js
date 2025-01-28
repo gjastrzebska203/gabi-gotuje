f800496092a61b811f7dff292ce5f5cea453a921
gjastrzebska203 <g.jastrzebska.203@studms.ug.edu.pl>
2025-01-28 14:30:46 +0100

    połączenie aplikacji z bazą

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const routes = require("./routes");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Połączono z bazą danych MongoDB Atlas"))
+  .catch((err) => console.error("Błąd połączenia z bazą danych:", err));
+
+app.use("/api", routes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Serwer działa na porcie ${PORT}`));
