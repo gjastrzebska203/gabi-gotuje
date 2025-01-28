9f6487b55634b81cdbe32ba5d0cda40f2106a1a3
gjastrzebska203 <g.jastrzebska.203@studms.ug.edu.pl>
2025-01-28 14:31:34 +0100

    modyfikacja index.js

---
+++ backend/routes/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const recipesRoutes = require("./recipes");
+const usersRoutes = require("./users");
+const commentsRoutes = require("./comments");
+const authRoutes = require("./auth");
+
+const router = express.Router();
+
+router.use("/recipes", recipesRoutes);
+router.use("/users", usersRoutes);
+router.use("/comments", commentsRoutes);
+router.use("/auth", authRoutes);
+
+module.exports = router;
