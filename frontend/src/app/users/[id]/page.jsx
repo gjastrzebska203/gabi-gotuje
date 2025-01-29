a423e6c578d42f1f6871fe1a8c10e29ef19a5331
gjastrzebska203 <g.jastrzebska.203@studms.ug.edu.pl>
2025-01-29 22:38:36 +0100

    dodano możliwość wyświetlania profilu innych użytkowników

---
+++ frontend/src/app/users/[id]/page.jsx
@@ -0,0 +1,46 @@
+"use client";
+import Navigation from "@/app/components/Navigation";
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import axios from "axios";
+
+export default function UserProfilePage() {
+  const { id } = useParams();
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    if (!id) return;
+
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL}/users/${id}`
+        );
+        setUser(response.data);
+      } catch (err) {
+        setError("Nie udało się pobrać danych użytkownika.");
+      }
+    };
+
+    fetchUser();
+  }, [id]);
+
+  if (error) return <p>{error}</p>;
+  if (!user) return <p>Ładowanie...</p>;
+
+  return (
+    <div className="page">
+      <h2>Profil użytkownika</h2>
+      <p>
+        <strong>Nazwa użytkownika:</strong> {user.username}
+      </p>
+      <p>
+        <strong>Email:</strong> {user.email}
+      </p>
+      <p>
+        <strong>Rola:</strong> {user.role || "Użytkownik"}
+      </p>
+    </div>
+  );
+}
